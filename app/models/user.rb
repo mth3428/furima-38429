984c6381de9725cd1a240411d5b9fccc36128c42
mth3428 <mth3428@gmail.com>
2022-11-01 21:23:06 +0900

    新規登録とログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true, format: { with: PASSWORD_REGEX, message:'には英字と数字の両方を含めて設定してください' }do
+    validates :password
+  end
+
 
   validates :nickname, presence: true
   validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  year_month_day = /\A\d{4}-\d{2}-\d{2}\z/
+  validates :birthday, presence: true, format: { with: year_month_day }
   
 end
