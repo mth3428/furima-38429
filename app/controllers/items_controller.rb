6860396778208e9430daeeaac94aed17ed655ad9
mth3428 <mth3428@gmail.com>
2022-11-06 17:50:57 +0900

    category_idの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,27 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
   private
+
+  def prototype_params
+    params.require(:item).permit(:item_name, :item_description, :category_id ).merge(user_id: current_user.id)
+  end
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
