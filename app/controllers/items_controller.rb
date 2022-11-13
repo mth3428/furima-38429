bfa9f1369ef5908ab06f6ec281a5c4309f158748
mth3428 <mth3428@gmail.com>
2022-11-13 20:00:52 +0900

    同じコードをまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: "DESC")
   end
@@ -20,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,5 +40,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_description, :category_id, :item_condition_id, :delivery_charge_id, :delivery_area_id, :delivery_date_id, :price, :image ).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
 end
